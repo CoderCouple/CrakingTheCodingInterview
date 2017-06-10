3ff23089dc4feb86d38412220d1732210b482927
Sunil Tiwari <sunil28071987@gmail.com>
2017-06-10 16:06:54 -0700

    Add files via upload

---
+++ Chapter1/OneAway.java
@@ -0,0 +1,17 @@
+package Chapter1;
+
+public class OneAway {
+
+	public static boolean isOnwAway(String str) {
+		
+		
+		
+		return false;
+
+	}
+
+	public static void main(String args[]) {
+
+	}
+
+}

6c32953e0ffd15331b93f86ddd37f5721e9dbe84
Sunil Tiwari <sunil28071987@gmail.com>
2017-06-12 16:39:06 -0700

    Create isUnique_v1.java

---
+++ Chapter1/isUnique_v1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class isUnique {
+
+	static String inputString = new String();
+
+	public static boolean isUniqueString(String s) {
+
+		inputString = s;
+		if (inputString.length() == 0) {
+			System.out.println("This is sn empty String..!!!");
+			return true;
+		}
+
+		for (int i = 0; i < inputString.length() - 1; i++) {
+			for (int j = i + 1; j < inputString.length(); j++) {
+				if (s.charAt(i) == s.charAt(j))
+					return false;
+
+			}
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the String to be checked for the uniqueness");
+		String s = input.nextLine();
+		System.out.println("Unique :" + isUniqueString(s));
+	}
+

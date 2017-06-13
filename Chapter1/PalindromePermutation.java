9e1aae5ab998363cf10e6da84d3bd86e11ef1137
Sunil Tiwari <sunil28071987@gmail.com>
2017-06-13 13:17:20 -0700

    Update PalindromePermutation.java

--- Chapter1/PalindromePermutation.java
+++ Chapter1/PalindromePermutation.java
@@ -1,18 +1,37 @@
 package Chapter1;
 
+import java.util.Scanner;
+
 public class PalindromePermutation {
 
+	public static boolean isPalindrome(String s) {
+		String inputString = s.replaceAll("\\s+", "");
+		boolean truthCheck[] = new boolean[128];
 
-	public static boolean isPalindrome(String s){
-		String stringArray []= s.split(" ");
-		
-		return false;
-		
+		for (int i = 0; i < inputString.length(); i++) {
+			int value = inputString.charAt(i);
+			truthCheck[value] = !truthCheck[value];
+		}
+
+		int count = 0;
+		for (int i = 0; i < truthCheck.length; i++) {
+
+			if (count > 1)
+				return false;
+
+			if (truthCheck[i] == true) {
+				count++;
+			}
+		}
+		return true;
+
 	}
-	
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the String to be checked for the Plindrome");
+		String s = input.nextLine();
+		System.out.println("Palindrome :" + isPalindrome(s));
 
 	}
 

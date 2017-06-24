0c092693a02c46043e54b3eb0318a1d1c0c7ec8d
Sunil Tiwari <sunil28071987@gmail.com>
2017-06-24 13:16:25 -0700

    Create BinaryTree.java

---
+++ java.util.*/BinaryTree.java
@@ -0,0 +1,41 @@
+
+public class BinaryTree {
+
+	TNode root;
+
+	public BinaryTree() {
+		root = null;
+	}
+
+	public void add(int element) {
+		
+	}
+
+	public boolean search(int element) {
+		
+		return false;
+		
+	}
+	
+	public boolean remove(int element) {
+		
+		return false;
+	}
+	
+	public void inOrderTraversal(TNode root) {
+		
+	}
+	
+	public void preOrderTraversal(TNode root) {
+		
+	}
+	
+	public void postOrderTraversal(TNode root) {
+		
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}

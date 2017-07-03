2e69c90600445d84a94d244bfa45f9115899d5d6
Sunil Tiwari <sunil28071987@gmail.com>
2017-07-03 16:55:33 -0700

    Update 5 Multiply.java

--- Chapter8/5 Multiply.java
+++ Chapter8/5 Multiply.java
@@ -3,18 +3,20 @@ package Chapter8;
 public class Multiply {
 
 	public static int getMultiplication(int num1, int num2) {
-
-		if (num2 <= 0) {
+		int b=num1>num2 ? num1 : num2;
+		int s=num1<num2 ? num1 : num2;
+		
+		if (s <= 0) {
 			return 0;
 		}
 
-		return num1 + getMultiplication(num1, num2 - 1);
+		return b + getMultiplication(b, s-1);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println(getMultiplication(7, 7));
+		System.out.println(getMultiplication(7, 3));
 
 	}
 

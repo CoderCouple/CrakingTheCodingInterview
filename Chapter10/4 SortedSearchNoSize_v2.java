043c653d990fdbf11d54744799084e82c3ecf91c
Sunil Tiwari <sunil28071987@gmail.com>
2017-07-10 17:01:40 -0700

    Update 4 SortedSearchNoSize_v2.java

--- Chapter10/4 SortedSearchNoSize_v2.java
+++ Chapter10/4 SortedSearchNoSize_v2.java
@@ -28,24 +28,15 @@ public class SortedSearchNoSize {
 		System.out.println(" * ");
 
 		if (array[searchStartRange] <= element && element < array[searchEndRange]) {
-			binarySearch(element, array, searchStartRange, searchEndRange);
+			return binarySearch(element, array, searchStartRange, searchEndRange);
 		} else {
 			if (elementAt(searchFactor * 2) != -1) {
 				return SortedSearchNoSize(element, array, searchFactor * 2);
 			} else {
-				int x = searchFactor;
-				while (elementAt(x) != -1) {
-
-					if (array[x] == element)
-						return x;
-					else {
-						x++;
-					}
-				}
+				return getLastSectionElementBybinarySearch(element,array,searchFactor,searchFactor * 2);
 			}
 		}
 
-		return -1;
 	}
 
 	public static int binarySearch(int element, int array[], int lb, int ub) {
@@ -59,16 +50,38 @@ public class SortedSearchNoSize {
 		if (array[mid] == element) {
 			return mid;
 		} else if (element < array[mid]) {
-			binarySearch(element, array, lb, mid - 1);
+			return binarySearch(element, array, lb, mid - 1);
 		} else {
-			binarySearch(element, array, mid + 1, ub);
+			return binarySearch(element, array, mid + 1, ub);
+		}
+
+	}
+
+	public static int getLastSectionElementBybinarySearch(int element, int array[], int lb, int ub) {
+
+		if (lb > ub) {
+			return -1;
+		}
+
+		int mid = (ub + lb) / 2;
+
+		if (elementAt(mid) == element) {
+			return mid;
+		} else if (elementAt(mid) == -1) {
+			return getLastSectionElementBybinarySearch(element, array, lb, mid - 1);
+		} else if (elementAt(mid) != -1) {
+			if (elementAt(mid) < element)
+				return getLastSectionElementBybinarySearch(element, array, mid + 1, ub);
+			else {
+				return getLastSectionElementBybinarySearch(element, array, lb, mid - 1);
+			}
 		}
 
 		return -1;
 	}
 
 	public static void main(String[] args) {
-		int element = 13;
+		int element = 6;
 		int searchFactor = 2;
 		int array[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 9, 10, 11, 11, 12, 13, 14, 15 };
 		System.out.println("Element at index :" + SortedSearchNoSize(element, array, 2));

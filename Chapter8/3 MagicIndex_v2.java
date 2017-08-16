05bcef16354b1d6a63770eac2f32a37e03c1b973
Sunil Tiwari <sunil28071987@gmail.com>
2017-08-16 13:43:59 -0700

    Update 3 MagicIndex_v2.java

--- Chapter8/3 MagicIndex_v2.java
+++ Chapter8/3 MagicIndex_v2.java
@@ -1,21 +1,29 @@
 package Chapter8;
 
-public class MagicIndex {
+public class MagicIndex2 {
 
-	public static int getMagicIndex(int array[]) {
+	public static int getMagicIndex(int array[], int lb, int ub) {
+		int mid = (lb + ub) / 2;
 
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] == i) {
-				return i;
-			}
+		if (lb > ub)
+			return -1;
+
+		if (array[mid] == mid)
+			return mid;
+		else {
+			int r = getMagicIndex(array, mid + 1, ub);
+			int l = getMagicIndex(array, lb, mid - 1);
+
+			if (l >= 0)
+				return l;
+			return r;
 		}
-		return -1;
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[] = { -5, -2, -1, 1, 2, 5 };
-		System.out.println("magic Index is :" + getMagicIndex(a));
+		int a[] = { -5, 1, -1, 1, 2, 5 };
+		System.out.println("magic Index is :" + getMagicIndex(a, 0, a.length - 1));
 
 	}
 
